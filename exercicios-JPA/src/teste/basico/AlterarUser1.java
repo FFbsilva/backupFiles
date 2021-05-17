cccfce346f6ed7bcc45223b5d95ed6b9bb7c5e92
FFbsilva <felipe.fbsilva@gmail.com>
2021-05-17 15:13:37 -0300

    Usando o JPA para realizar update's

--- exercicios-JPA/src/teste/basico/AlterarUser1.java
+++ exercicios-JPA/src/teste/basico/AlterarUser1.java
@@ -4,6 +4,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import modelo.basico.Usuario;
+
 public class AlterarUser1 {
 public static void main(String[] args) {
 	
@@ -12,5 +14,17 @@ public static void main(String[] args) {
 	
 	EntityManager em = emf.createEntityManager();
 	
+	em.getTransaction().begin();
+	
+	Usuario usuario = em.find(Usuario.class, 3L);
+	//System.out.println(usuario.getNome());
+	usuario.setEmail("chiquinho_low@gmail.com");
+	usuario.setNome("Chiquinho da Sul");
+	em.merge(usuario);
+	
+	em.getTransaction().commit();
+	
+	em.close();
+	emf.close();
 }
 }
